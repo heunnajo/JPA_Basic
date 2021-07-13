31d373cbb9e0dd257b45a29d81c95fc5d2d64345
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-13 21:17:42 +0900

    EntityManager finds data from 1st cache NOT from database.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,10 +4,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");//설정파일에서 설정한 이름 가져온다
 
         EntityManager em = emf.createEntityManager();
 
@@ -16,17 +17,23 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            member.setId(101L);
+            member.setName("HelloHello");
 
+            System.out.println("===== BEFORE =====");
             em.persist(member);
+            System.out.println("===== AFTER =====");
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
             tx.commit();
         } catch (Exception e){
             tx.rollback();
         } finally {
-            em.close();//em이 결국 DB 연결을 담당하기 때문에 작업 끝나면 닫아줘야한다!!
+            em.close();//em이 결국 DB 연결을 담당하기 때문에 자원을 다 쓰고 작업 끝나면 닫아줘야한다!!
         }
-        emf.close();
-
+        emf.close();//애플리케이션 종료 또는 WAS 내려갈 때 emf도 닫아준다!
     }
 }
