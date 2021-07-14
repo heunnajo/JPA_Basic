73b68fa95f58da406a45dfcbe98464de4a23c683
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-14 09:05:51 +0900

    Checked DB Schema auto-generate option
    1. create
    2. create-drop
    3. update
    4. validate
    5. none

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,11 +17,10 @@ public class JpaMain {
 
         try {
             //영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
+            //101	HelloHello를 바꿔보자.
+            Member member = em.find(Member.class, 101L);
+            member.setName("XYZ");
 
-            em.persist(member1);
-            em.persist(member2);
             System.out.println("============================");//이 라인 기준으로 쿼리가 언제 나가는지 구분!
             tx.commit();
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -8,7 +8,8 @@ public class Member {
     @Id//JPA에 PK 알려준다!!!
     private Long id;
     private String name;
-
+    private int age;
+    private int gogo;
     //JPA는 리플렉션해야해서 동적으로 객체 생성해야하기 때문에 기본 생성자 있어야 한다!
     public Member() {
     }
